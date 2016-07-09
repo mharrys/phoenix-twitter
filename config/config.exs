6485e08c9bd0ab103c1dc2cdcfc67af4c7866b8d
Mattias Harrysson <mathar@kth.se>
2016-07-09 17:12:14 +0200

    init

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :app,
+  ecto_repos: [App.Repo]
+
+# Configures the endpoint
+config :app, App.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "jAcyws/tztKIPTDAx6FsgrxO2ysmSRz0GOwbKU9wc84irCs1ti3zaC783VYOvKJM",
+  render_errors: [view: App.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: App.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"

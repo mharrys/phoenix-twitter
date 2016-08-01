9cfd9b693de424c001c16bbbaf2d0db78914670c
Mattias Harrysson <mathar@kth.se>
2016-08-01 19:07:30 +0200

    add nicer looking error templates

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,20 +1,6 @@
 defmodule App.ErrorView do
   use App.Web, :view
 
-  def render("401.html", _assigns) do
-    "Unauthorized"
-  end
-
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
-
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
     render "500.html", assigns
   end

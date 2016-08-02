fa1bbd6acd637862999409d194481bdf09cf7b59
Mattias Harrysson <mathar@kth.se>
2016-08-02 11:58:28 +0200

    do not repeat common query details

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -10,21 +10,17 @@ defmodule App.PageController do
   @num_users  5
 
   def index(conn, _params) do
-    tweets = case get_session conn, :current_user do
+    query = Tweet |> order_by([t], [desc: t.inserted_at]) |> limit([t], @num_tweets)
+    query = case get_session conn, :current_user do
       nil ->
-        Repo.all Tweet
-        |> order_by([t], [desc: t.inserted_at])
-        |> limit([t], @num_tweets)
-        |> preload(:user)
+        query
       current_user ->
-        Repo.all Tweet
-        |> order_by([t], [desc: t.inserted_at])
-        |> limit([t], @num_tweets)
+        query
         |> join(:left, [t], f in Favorite, f.user_id == ^current_user.id and f.tweet_id == t.id)
         |> join(:left, [t, f], r in Retweet, r.user_id == ^current_user.id and r.tweet_id == t.id)
         |> select([t, f, r], %{t | current_user_favorite_id: f.id, current_user_retweet_id: r.id})
     end
-    tweets = tweets |> Repo.preload(:user)
+    tweets = Repo.all(query) |> Repo.preload(:user)
     users = Repo.all from t in User, order_by: [desc: t.inserted_at], limit: @num_users
     render conn, "index.html", tweets: tweets, users: users
   end

f4cea220ea72df5172b2bc6c370bed58ac8d8a16
Mattias Harrysson <mathar@kth.se>
2016-08-02 11:37:25 +0200

    do not allow users to retweet their own tweets

--- web/controllers/retweet_controller.ex
+++ web/controllers/retweet_controller.ex
@@ -9,10 +9,17 @@ defmodule App.RetweetController do
   def create(conn, %{"tweet_id" => tweet_id}) do
     current_user = conn.assigns[:current_user]
     tweet = Repo.get! Tweet, tweet_id
-    retweet_param = %{tweet_id: tweet.id, user_id: current_user.id}
-    changeset = Retweet.changeset(%Retweet{}, retweet_param)
-    Repo.insert! changeset
-    redirect conn, to: user_path(conn, :show, current_user.id)
+    if tweet.user_id === current_user.id do
+      conn
+      |> put_flash(:error, "You are not allowed to retweet your own tweets")
+      |> redirect(to: user_path(conn, :show, current_user.id))
+      |> halt
+    else
+      retweet_param = %{tweet_id: tweet.id, user_id: current_user.id}
+      changeset = Retweet.changeset(%Retweet{}, retweet_param)
+      Repo.insert! changeset
+      redirect conn, to: user_path(conn, :show, current_user.id)
+    end
   end
 
   def delete(conn, %{"tweet_id" => tweet_id}) do

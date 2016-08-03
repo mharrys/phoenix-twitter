f0b89d345d6c88876a6432c4670414f91445680f
Mattias Harrysson <mathar@kth.se>
2016-08-03 15:16:09 +0200

    add layout menu test

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,12 @@
 defmodule App.LayoutViewTest do
   use App.ConnCase, async: true
+
+  test "menu options when not signed in", %{conn: conn} do
+    content = conn
+    |> get("/")
+    |> html_response(200)
+    Enum.map(["Users", "Tweets", "Signup", "Login"], fn item ->
+      assert content =~ item
+    end)
+  end
 end

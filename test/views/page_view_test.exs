8630dacd5ffb80c7bc9e9a07bcc73854509c4651
Mattias Harrysson <mathar@kth.se>
2016-08-06 13:30:01 +0200

    page controller test is now a page view test

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,10 @@
 defmodule App.PageViewTest do
   use App.ConnCase, async: true
+
+  test "if displaying latest users and tweets", %{conn: conn} do
+    content = conn |> get(page_path(conn, :index)) |> html_response(200)
+    Enum.map(["Latest Users", "Latest Tweets"], fn item ->
+      assert content =~ item
+    end)
+  end
 end
